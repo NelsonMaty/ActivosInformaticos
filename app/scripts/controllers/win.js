cea87346700f9dd7ddf5b0c3469ad67484031cc6
Gonzalo Arias <garias@unc.edu.ar>
2016-01-12 10:48:03 -0300

    Agregando intento de redireccion en menu de usuario administrador

--- app/scripts/controllers/win.js
+++ app/scripts/controllers/win.js
@@ -1,6 +1,6 @@
 
 angular.module('activosInformaticosApp')
-  .controller('AppController', function ($scope, $mdDialog) {
+  .controller('AppController', function ($scope, $mdDialog, $location) {
     //var vm = this;
     
     $scope.toggleSidenav = function(menuId) {
@@ -11,6 +11,11 @@ angular.module('activosInformaticosApp')
       originatorEv = ev;
       $mdOpenMenu(ev);
     };
+
+    $scope.go = function(path) {
+      //location.href = "0.0.0.0:9000/#/admin";
+      $location.path( path );
+    }
 
     
   });
